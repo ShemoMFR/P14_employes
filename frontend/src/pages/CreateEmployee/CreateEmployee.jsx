1b7c3cde354c09a08fb404ddb2f6aa513b079e89
Shemo <moshe.frydmann@gmail.com>
2022-01-25 19:14:50 +0200

    html createEmployee

--- frontend/src/pages/CreateEmployee/CreateEmployee.jsx
+++ frontend/src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,6 +1,7 @@
 /* LIBRAIRIES */
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 /* CSS */
 import './CreateEmployee.css';
@@ -10,9 +11,56 @@ const CreateEmployee = () => {
     const {register, handleSubmit, formState: { errors }, getValues} = useForm();
 
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="title">
+                <h1>HRnet</h1>
+            </div>
+            <div className="container">
+                 <Link to='/employee-list'>View Current Employees</Link>
+                <h2>Create Employee</h2>
+                <form id="create-employee">
+                     <label htmlFor="first-name">First Name</label>
+                    <input type="text" id="first-name" />
+
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" id="last-name" />
+
+                    <label htmlFor="date-of-birth">Date of Birth</label>
+                    <input id="date-of-birth" type="text" />
+
+                    <label htmlFor="start-date">Start Date</label>
+                    <input id="start-date" type="text" />
+
+                    <fieldset className="address">
+                        <legend>Address</legend>
+
+                        <label htmlFor="street">Street</label>
+                        <input id="street" type="text" />
+
+                        <label htmlFor="city">City</label>
+                        <input id="city" type="text" />
+
+                        <label htmlFor="state">State</label>
+                        <select name="state" id="state"></select>
+
+                        <label htmlFor="zip-code">Zip Code</label>
+                        <input id="zip-code" type="number" />
+                    </fieldset>
+
+                    <label htmlFor="department">Department</label>
+                    <select name="department" id="department">
+                        <option>Sales</option>
+                        <option>Marketing</option>
+                        <option>Engineering</option>
+                        <option>Human Resources</option>
+                        <option>Legal</option>
+                    </select> 
+                </form>
+
+                <button >Save</button> 
+            </div>
+            <div id="confirmation" className="modal">Employee Created!</div>
+        </>
     )
 };
 
