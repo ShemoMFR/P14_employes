ad1f96202c48c21f5421d4fc9d280ad522088715
Shemo <moshe.frydmann@gmail.com>
2022-01-26 19:14:57 +0200

    Table

--- frontend/src/pages/EmployeeList/EmployeeList.jsx
+++ frontend/src/pages/EmployeeList/EmployeeList.jsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/* COMPONENTS */
+import DataTable from '../../components/table'
+
 /* CSS */
 import './EmployeeList.css';
 
@@ -9,7 +12,7 @@ const EmployeeList = () => {
     return (
         <div id="employee-div" className="container">
             <h1>Current Employees</h1>
-            <table id="employee-table" className="display"></table>
+            <DataTable />
             <Link to='/'>Home</Link>
         </div>
     )
